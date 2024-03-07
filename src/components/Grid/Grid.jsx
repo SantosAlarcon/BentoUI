4d7dccca6332cb064d845faa09eb7e37f01cc387
Santos Alarcón <santosalarcon86@gmail.com>
2024-03-07 19:42:18 +0100

    Primer commit

---
+++ src/components/Grid/Grid.jsx
@@ -0,0 +1,40 @@
+import "./Grid.css"
+
+const Grid = () => {
+    return (
+        <div className="grid__layout">
+            <div className="grid__layout__gato1">
+                <img src="/1.webp" alt="Gato1" />
+            </div>
+            <div className="grid__layout__gato2">
+                <img src="/2.webp" alt="Gato2" />
+            </div>
+            <div className="grid__layout__gato3">
+                <img src="/3.webp" alt="Gato3" />
+            </div>
+            <div className="grid__layout__gato4">
+                <img src="/4.webp" alt="Gato4" />
+            </div>
+            <div className="grid__layout__gato5">
+                <img src="/5.webp" alt="Gato5" />
+            </div>
+            <div className="grid__layout__gato6">
+                <img src="/6.webp" alt="Gato6" />
+            </div>
+            <div className="grid__layout__gato7">
+                <img src="/7.webp" alt="Gato7" />
+            </div>
+            <div className="grid__layout__gato8">
+                <img src="/8.webp" alt="Gato8" />
+            </div>
+            <div className="grid__layout__gato9">
+                <img src="/9.webp" alt="Gato9" />
+            </div>
+            <div className="grid__layout__gato10">
+                <img src="/10.webp" alt="Gato10" />
+            </div>
+        </div>
+    )
+}
+
+export default Grid
